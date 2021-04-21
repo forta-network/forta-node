aee8d4a42a7e22137c2be2427065da572cf8fb16
Steven Landers <steven.landers@gmail.com>
2021-04-21 06:18:10 -0400

    add block iterator test

---
+++ feeds/blocks_test.go
@@ -0,0 +1,143 @@
+package feeds
+
+import (
+	"context"
+	"errors"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	clients "OpenZeppelin/safe-node/clients/mocks"
+	"OpenZeppelin/safe-node/utils"
+)
+
+var testErr = errors.New("test")
+var startHash = "0x4fc0862e76691f5312964883954d5c2db35e2b8f7a4f191775a4f50c69804a8d"
+var reorgHash = "0xb9b293da464be42bbb87695c372678ea93a2ef87dc54213bbaa93bd6d8880c17"
+
+func mockFeed(t *testing.T) (*blockFeed, *clients.MockEthClient, context.Context, context.CancelFunc) {
+	ctrl := gomock.NewController(t)
+	client := clients.NewMockEthClient(ctrl)
+	ctx, cancel := context.WithCancel(context.Background())
+	cache := utils.NewCache(10000)
+	return &blockFeed{
+		start:  big.NewInt(1),
+		ctx:    ctx,
+		client: client,
+		cache:  cache,
+	}, client, ctx, cancel
+}
+
+func blockWithParent(hash string, num int) *types.Block {
+	return types.NewBlockWithHeader(&types.Header{
+		ParentHash: common.HexToHash(hash),
+		Number:     big.NewInt(int64(num)),
+	})
+}
+
+func blockEvent(blk *types.Block) *BlockEvent {
+	return &BlockEvent{
+		EventType: EventTypeBlock,
+		Block:     blk,
+	}
+}
+
+func reorgEvent(blk *types.Block) *BlockEvent {
+	return &BlockEvent{
+		EventType: EventTypeReorg,
+		Block:     blk,
+	}
+}
+
+func assertEvts(t *testing.T, actual []*BlockEvent, expected ...*BlockEvent) {
+	assert.Equal(t, len(actual), len(expected), "expect same length")
+	for i, exp := range expected {
+		assert.Equal(t, exp, actual[i])
+	}
+}
+
+func TestBlockFeed_ForEachBlock(t *testing.T) {
+	bf, client, ctx, _ := mockFeed(t)
+
+	block1 := blockWithParent(startHash, 1)
+	block2 := blockWithParent(block1.Hash().Hex(), 2)
+	block3 := blockWithParent(block2.Hash().Hex(), 3)
+
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(1)).Return(block1, nil).Times(1)
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(2)).Return(block2, nil).Times(1)
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(3)).Return(block3, nil).Times(1)
+
+	count := 0
+	var evts []*BlockEvent
+	res := bf.ForEachBlock(func(evt *BlockEvent) error {
+		count++
+		evts = append(evts, evt)
+		if count == 3 {
+			return testErr
+		}
+		return nil
+	})
+	assert.Error(t, testErr, res)
+	assert.Equal(t, 3, len(evts))
+	assertEvts(t, evts, blockEvent(block1), blockEvent(block2), blockEvent(block3))
+}
+
+func TestBlockFeed_ForEachBlock_Cancelled(t *testing.T) {
+	bf, client, ctx, cancel := mockFeed(t)
+
+	hash1 := "0x4fc0862e76691f5312964883954d5c2db35e2b8f7a4f191775a4f50c69804a8d"
+	block1 := types.NewBlockWithHeader(&types.Header{
+		ParentHash: common.HexToHash(hash1),
+	})
+
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(1)).Return(block1, nil).Times(1)
+
+	count := 0
+	var evts []*BlockEvent
+	res := bf.ForEachBlock(func(evt *BlockEvent) error {
+		count++
+		evts = append(evts, evt)
+		cancel()
+		return nil
+	})
+	assert.Error(t, context.Canceled, res)
+	assert.Equal(t, 1, len(evts))
+	assertEvts(t, evts, blockEvent(block1))
+}
+
+func TestBlockFeed_ForEachBlock_Reorg(t *testing.T) {
+	bf, client, ctx, _ := mockFeed(t)
+
+	// START
+	block1 := blockWithParent(startHash, 1)
+
+	// Different Parent!
+	reorg := blockWithParent(reorgHash, 1)
+	// Reorg...Its Parent is START, found common ancestry (exists in cache)
+	block2 := blockWithParent(block1.Hash().Hex(), 2)
+	// And Continue
+	block3 := blockWithParent(block2.Hash().Hex(), 3)
+
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(1)).Return(block1, nil).Times(1)
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(2)).Return(reorg, nil).Times(1)
+	client.EXPECT().BlockByHash(ctx, reorg.ParentHash()).Return(block2, nil).Times(1)
+	client.EXPECT().BlockByNumber(ctx, big.NewInt(3)).Return(block3, nil).Times(1)
+
+	count := 0
+	var evts []*BlockEvent
+	res := bf.ForEachBlock(func(evt *BlockEvent) error {
+		count++
+		evts = append(evts, evt)
+		if count == 4 {
+			return testErr
+		}
+		return nil
+	})
+	assert.Error(t, testErr, res)
+	assert.Equal(t, 4, count)
+	assertEvts(t, evts, blockEvent(block1), blockEvent(reorg), reorgEvent(block2), blockEvent(block3))
+}
--- go.mod
+++ go.mod
@@ -6,21 +6,21 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/Microsoft/go-winio v0.4.17 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
-	github.com/cenkalti/backoff/v4 v4.1.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.0
 	github.com/d4l3k/messagediff v1.2.1 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v1.4.2-0.20180625184442-8e610b2b55bf
 	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/ethereum/go-ethereum v1.10.2
-	github.com/google/uuid v1.2.0 // indirect
+	github.com/golang/mock v1.3.1
+	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tylertreat/BoomFilters v0.0.0-20210315201527-1a82519a3e43
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
