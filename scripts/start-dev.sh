90e92564827598dfcfefe988bb3742959e0ab927
Steven Landers <steven.landers@gmail.com>
2021-06-03 19:07:49 -0400

    add permissions

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,8 +2,9 @@
 
 GITHUB_HASH=$1
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 997179694723.dkr.ecr.us-west-2.amazonaws.com
-killall -s SIGINT fortify
 docker pull 997179694723.dkr.ecr.us-west-2.amazonaws.com/fortify-scanner:$GITHUB_HASH
 docker pull 997179694723.dkr.ecr.us-west-2.amazonaws.com/fortify-query:$GITHUB_HASH
 docker pull 997179694723.dkr.ecr.us-west-2.amazonaws.com/fortify-json-rpc:$GITHUB_HASH
+killall -s SIGINT fortify
+sleep 3
 nohup ./fortify -passphrase $PASSPHRASE &
