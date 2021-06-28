e8946e058aae7892e72659f12fe3d20494ee058b
Steven Landers <steven.landers@gmail.com>
2021-06-28 18:55:05 -0400

    remove blank line

--- scripts/load-images.sh
+++ scripts/load-images.sh
@@ -6,7 +6,6 @@ docker pull "ECR_REGISTRY/fortify-scanner:GITHUB_HASH"
 docker pull "ECR_REGISTRY/fortify-query:GITHUB_HASH"
 docker pull "ECR_REGISTRY/fortify-json-rpc:GITHUB_HASH"
 
-
 instanceId=$(curl -s http://instance-data/latest/meta-data/instance-id)
 region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 envPrefix=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=Environment" |jq -r '.Tags[0].Value')
