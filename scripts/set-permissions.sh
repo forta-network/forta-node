3b6e5da89bb86baca8e0787dc1d35317fece7278
Steven Landers <steven.landers@gmail.com>
2021-06-28 11:17:44 -0400

    add codedeploy deploy

---
+++ scripts/set-permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chown fortify.fortify /usr/bin/fortify
+chmod 755 /usr/bin/fortify
---
+++ scripts/start-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# dynamically look up the secret name
+instanceId=$(curl -s http://instance-data/latest/meta-data/instance-id)
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+envPrefix=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=Environment" |jq -r '.Tags[0].Value')
+secretId="${envPrefix}_fortify_passphrase"
+
+# get secret from secrets manager
+passphrase=$(aws secretsmanager --region $region get-secret-value --secret-id $secretId |jq -r '.SecretString')
+nohup fortify -config /etc/fortify/config.yml -passphrase $passphrase > /dev/null 2> /dev/null < /dev/null &
