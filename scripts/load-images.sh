1e8e7846652086cdf4acea2d5d0cd2acb0645960
Steven Landers <steven.landers@gmail.com>
2021-06-28 18:39:09 -0400

    attempt to load images

--- scripts/load-images.sh
+++ scripts/load-images.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-accountId=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .accountId)
+# ECR_REGISTRY and GITHUB_HASH are replaced during code deploy
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin "ECR_REGISTRY"
+docker pull "ECR_REGISTRY/fortify-scanner:GITHUB_HASH"
+docker pull "ECR_REGISTRY/fortify-query:GITHUB_HASH"
+docker pull "ECR_REGISTRY/fortify-json-rpc:GITHUB_HASH"
 
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin "${accountId}.dkr.ecr.${region}.amazonaws.com"
-docker pull "${accountId}.dkr.ecr.${region}.amazonaws.com/fortify-scanner:GITHUB_HASH"
-docker pull "${accountId}.dkr.ecr.${region}.amazonaws.com/fortify-query:GITHUB_HASH"
-docker pull "${accountId}.dkr.ecr.${region}.amazonaws.com/fortify-json-rpc:GITHUB_HASH"
+
+instanceId=$(curl -s http://instance-data/latest/meta-data/instance-id)
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+envPrefix=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=Environment" |jq -r '.Tags[0].Value')
+
+# pull agents by parsing config file
+cat "/etc/fortify/config-fortify-${envPrefix}.yml" | grep image | sed -e 's/.* //g'|xargs -I {} docker pull {}
