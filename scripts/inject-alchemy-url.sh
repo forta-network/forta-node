93643e9162b36e91d3994eb5dc42d70dedb32f9b
Steven Landers <steven.landers@gmail.com>
2021-07-01 15:45:13 -0400

    escape url so sed replace works

--- scripts/inject-alchemy-url.sh
+++ scripts/inject-alchemy-url.sh
@@ -6,6 +6,7 @@ region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/documen
 envPrefix=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=Environment" |jq -r '.Tags[0].Value')
 secretId="${envPrefix}_alchemy_api_url"
 
-apiUrl=$(aws secretsmanager --region $region get-secret-value --secret-id $secretId |jq -r '.SecretString')
+apiUrlUnsafe=$(aws secretsmanager --region $region get-secret-value --secret-id $secretId |jq -r '.SecretString')
+apiUrl=$(printf '%s\n' "$apiUrlUnsafe" | sed -e 's/[]\/$*.^[]/\\&/g');
 
 sed -i "s/ALCHEMY_URL/$apiUrl/g" /etc/fortify/config-fortify-dev.yml
