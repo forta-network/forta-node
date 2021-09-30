e26de8b63399efcfde2aadc3e74caac554090e3f
Caner Çıdam <canercidam01@gmail.com>
2021-10-01 01:56:27 +0300

    lazy private key initialization and using different configs

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -8,5 +8,32 @@ secretId="${envPrefix}_forta_passphrase"
 
 # get secret from secrets manager
 passphrase=$(aws secretsmanager --region $region get-secret-value --secret-id $secretId |jq -r '.SecretString')
+
+# get private key JSON from DynamoDB
+privateKeysTable="$envPrefix-forta-node-private-keys"
+nodeName=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=Name" | jq -r '.Tags[0].Value')
+privateKeyItem=$(aws dynamodb get-item --region $region --table $privateKeysTable --key '{"NodeName": { "S": '"$nodeName"' }}' | jq -r .Item)
+privateKeyFileName=''
+# create and store new one if it doesn't exist
+if [ -z "$privateKeyItem" ]; then
+	wget -O geth.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.9-eae3b194.tar.gz
+	tar -xvf geth.tar.gz
+	./geth/geth account new --password <(echo $passphrase) --keystore "$HOME/.forta/.keys"
+	privateKeyFileName=$(ls ~/.forta/.keys | head -n 1)
+	privateKeyJson=$(cat "$HOME/.forta/.keys/$privateKeyFileName")
+	ethereumAddress="0x$(echo $privateKeyJson | jq -r .address)"
+	privateKeyJson=$(echo $privateKeyJson | jq -RM) # escape so we can put it into another JSON
+	dynamoItemTpl='{NodeName:{S:$name},EthereumAddress:{S:$address},PrivateKeyJson:{S:$privKeyJson},FileName:{S:$keyFileName}}'
+	privateKeyItem=$(jq -ncM --arg name "$nodeName" --arg address "$ethereumAddress" --arg privKeyJson "$privateKeyJson" --arg keyFileName "$privateKeyFileName" "$dynamoItemTpl")
+    aws dynamodb put-item --region $region --table-name $privateKeysTable --item "$privateKeyItem"
+fi
+privateKeyJson=$(echo $privateKeyItem | jq -r '.PrivateKeyJson.S')
+privateKeyFileName=$(echo $privateKeyItem | jq -r '.FileName.S')
+# write the private key file to ensure it exists in the right place
+cat "$privateKeyJson" > "$HOME/.forta/.keys/$privateKeyFileName"
+
+# get config file name
+configFileName=$(aws ec2 describe-tags --region $region --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=FortaConfig" | jq -r '.Tags[0].Value')
+
 nohup \
-	forta --config "/etc/forta/config.yml" --passphrase $passphrase run > /dev/null 2> /tmp/forta.log < /dev/null &
+	forta --config "/etc/forta/configs/$configFileName" --passphrase $passphrase run > /dev/null 2> /tmp/forta.log < /dev/null &
