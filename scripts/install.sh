b14bf6b87a660bc50130fb4616802038360cc357
Caner Çıdam <canercidam01@gmail.com>
2021-10-07 00:56:10 +0300

    fix install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 # install geth
 wget -O /tmp/geth.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.9-eae3b194.tar.gz
 tar -xvzf /tmp/geth.tar.gz
-cp /tmp/geth-linux*/geth /usr/bin/geth
+cp geth-linux*/geth /usr/bin/geth
 chmod +x /usr/bin/geth
