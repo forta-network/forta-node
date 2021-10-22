ce4af3a5c5d3c2ca591474bf3b48f0c3ece3d45e
Steven Landers <steven.landers@gmail.com>
2021-10-22 08:09:26 -0400

    try 0x fix

--- utils/convert_test.go
+++ utils/convert_test.go
@@ -12,3 +12,8 @@ func TestHexToBigInt(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int64(2748), res.Int64())
 }
+
+func TestBytesToHex(t *testing.T) {
+	value := "ABCDEFG12382837581235uASDFASDFASDFzxcvzxcvzxcv"
+	assert.Equal(t, "0x37353831323335754153444641534446415344467a7863767a7863767a786376", BytesToHex([]byte(value)))
+}
