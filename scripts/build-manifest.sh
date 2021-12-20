fcdf8ee866c82c97bc8bc71ed04f61692da93737
Steven Landers <steven.landers@gmail.com>
2021-12-20 10:22:41 -0500

    strip repository

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -2,7 +2,10 @@
 
 cp $1 $2
 
+# strip the repository from path
+img=$(echo "$4" | sed "s/.*\///")
+
 ts=$(date --utc +%FT%T.%3NZ)
-sed -i "s/%TIMESTAMP%/$ts/g" $2
-sed -i "s/%COMMIT_SHA%/$3/g" $2
-sed -i "s/%NODE_IMAGE%/$4/g" $2
+sed -i "s|%TIMESTAMP%|$ts|g" $2
+sed -i "s|%COMMIT_SHA%|$3|g" $2
+sed -i "s|%NODE_IMAGE%|$img|g" $2
