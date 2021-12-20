b576995d697dadb69627b9ece5f088bfa586fe71
Steven Landers <steven.landers@gmail.com>
2021-12-20 10:01:55 -0500

    try with file

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -3,6 +3,6 @@
 cp $1 $2
 
 ts=$(date --utc +%FT%T.%3NZ)
-sed -i "s/%TIMESTAMP%/$ts/g" manifest.json
-sed -i "s/%COMMIT_SHA%/$3/g" manifest.json
-sed -i "s/%NODE_IMAGE%/$4/g" manifest.json
+sed -i "s/%TIMESTAMP%/$ts/g" $2
+sed -i "s/%COMMIT_SHA%/$3/g" $2
+sed -i "s/%NODE_IMAGE%/$4/g" $2
