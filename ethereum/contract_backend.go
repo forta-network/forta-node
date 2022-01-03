fbb6c57efbec24ea73f12b5a25352d8e2d8226e2
Caner Çıdam <canercidam01@gmail.com>
2022-01-03 16:09:44 +0300

    add more logging to SentTransaction and PendingNonceAt

--- ethereum/contract_backend.go
+++ ethereum/contract_backend.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
+	log "github.com/sirupsen/logrus"
 )
 
 // ContractBackend is the same interface.
@@ -29,25 +30,47 @@ func NewContractBackend(client *rpc.Client) bind.ContractBackend {
 
 // PendingNonceAt helps us count the nonce more robustly.
 func (cb *contractBackend) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
+	logger := log.WithField("address", account.Hex())
 	pendingNonce, err := cb.ContractBackend.PendingNonceAt(ctx, account)
 	if err != nil {
+		logger.WithError(err).Error("failed to get pending nonce from server")
 		return 0, err
 	}
+	logger = logger.WithFields(log.Fields{
+		"serverNonce": pendingNonce,
+		"localNonce":  cb.nonce,
+	})
 	if pendingNonce > cb.nonce {
+		logger.Info("using server nonce")
 		return pendingNonce, nil
 	}
+	logger.Info("using local nonce")
 	return cb.nonce, nil
 }
 
 // SendTransaction sends the transaction with the most up-to-date nonce.
 func (cb *contractBackend) SendTransaction(ctx context.Context, tx *types.Transaction) error {
+	logger := getTxLogger(tx)
+	logger.Info("sending")
 	if err := cb.ContractBackend.SendTransaction(ctx, tx); err != nil {
+		logger.WithError(err).Error("failed to send")
 		return err
 	}
+	logger.Info("sent")
 	// count it locally: if sending the tx is successful than that's the previous nonce for sure
 	newNonce := tx.Nonce() + 1
 	if newNonce > cb.nonce {
 		cb.nonce = newNonce
 	}
 	return nil
+}
+
+func getTxLogger(tx *types.Transaction) *log.Entry {
+	return log.WithFields(log.Fields{
+		"to":       tx.To().Hex(),
+		"nonce":    tx.Nonce(),
+		"gasLimit": tx.Gas(),
+		"gasPrice": tx.GasPrice().Uint64(),
+		"hash":     tx.Hash().Hex(),
+	})
 }
