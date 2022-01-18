97a4d6cb1eb83d23e56c3a5e78fac94e219ce781
Caner Çıdam <canercidam01@gmail.com>
2022-01-19 00:28:36 +0300

    handle more permanent errors (trace)

--- ethereum/client.go
+++ ethereum/client.go
@@ -53,6 +53,8 @@ var permanentErrors = []string{
 	"method not found",
 	"hash is not currently canonical",
 	"unknown block",
+	"Unable to complete request at this time",
+	"503 Service Unavailable",
 }
 
 var minBackoff = 1 * time.Second
