47286e77152c6811a2bd16cd1557d82946a7d724
Caner Çıdam <canercidam01@gmail.com>
2022-02-18 17:08:33 +0300

    add debugging line

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -6,6 +6,7 @@ REGISTRY="$1"
 IMAGE_NAME="$2"
 FULL_IMAGE_NAME="$REGISTRY/forta-$IMAGE_NAME"
 
+cat ethereum/client.go
 docker build -t "$FULL_IMAGE_NAME" -f "Dockerfile.$IMAGE_NAME" . > /dev/null
 PUSH_OUTPUT=$(docker push "$FULL_IMAGE_NAME")
 DIGEST=$(echo "$PUSH_OUTPUT" | grep -oE '([0-9a-z]{64})')
