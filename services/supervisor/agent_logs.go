5a1d26481dee24c6230598e84893c5b0fe1d844f
Caner Çıdam <canercidam01@gmail.com>
2022-02-23 01:11:13 +0300

    make some adjustments & require agent logs label to enable it

--- services/supervisor/agent_logs.go
+++ services/supervisor/agent_logs.go
@@ -5,19 +5,20 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/forta-protocol/forta-node/clients"
 	"github.com/forta-protocol/forta-node/clients/agentlogs"
 	log "github.com/sirupsen/logrus"
 )
 
 // adjust these better with auto-upgrade later
 const (
-	defaultAgentLogSendInterval = time.Minute
-	defaultAgentLogTailLines    = 20
-	defaultAgentLogMaxLength    = 4096
+	defaultAgentLogSendInterval       = time.Minute
+	defaultAgentLogTailLines          = 50
+	defaultAgentLogAvgMaxCharsPerLine = 200
 )
 
 func (sup *SupervisorService) syncAgentLogs() {
-	time.After(time.Second * 15) // rate limit crash loops
+	time.After(time.Minute) // rate limit crash loops
 	ticker := time.NewTicker(defaultAgentLogSendInterval)
 	for range ticker.C {
 		err := sup.doSyncAgentLogs()
@@ -41,7 +42,19 @@ func (sup *SupervisorService) doSyncAgentLogs() error {
 		if !container.IsAgent {
 			continue
 		}
-		logs, err := sup.client.GetContainerLogs(sup.ctx, container.ID, strconv.Itoa(defaultAgentLogTailLines), defaultAgentLogMaxLength)
+		dockerContainer, err := sup.client.GetContainerByID(sup.ctx, container.ID)
+		if err != nil {
+			log.WithError(err).Warn("failed to get agent container")
+			continue
+		}
+		if dockerContainer.Labels[clients.DockerLabelFortaSettingsAgentLogsEnabled] != "true" {
+			continue
+		}
+		logs, err := sup.client.GetContainerLogs(
+			sup.ctx, container.ID,
+			strconv.Itoa(defaultAgentLogTailLines),
+			defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+		)
 		if err != nil {
 			log.WithError(err).Warn("failed to get agent container logs")
 			continue
