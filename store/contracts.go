705c22e7c92de92390152b2c50db3ecace3ea146
Steven Landers <steven.landers@gmail.com>
2022-02-27 16:35:24 -0500

    remove more files

--- store/contracts.go
+++
@@ -1,9 +0,0 @@
-package store
-
-import (
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-)
-
-type scannerNodeVersion interface {
-	ScannerNodeVersion(opts *bind.CallOpts) (string, error)
-}
