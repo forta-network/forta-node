5fbd1be579f8e130ade6d1ffaef15e8efef71071
Caner Çıdam <canercidam01@gmail.com>
2022-06-10 01:54:56 +0300

    detect newer releases while waiting for update delay

--- services/updater/updater_test.go
+++ services/updater/updater_test.go
@@ -5,59 +5,96 @@ import (
 	"testing"
 
 	"github.com/forta-network/forta-core-go/release"
+	"github.com/stretchr/testify/require"
 
 	rm "github.com/forta-network/forta-core-go/registry/mocks"
 	im "github.com/forta-network/forta-core-go/release/mocks"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
-	testDefaultCheckIntervalSeconds = 60
+	testUpdateCheckIntervalSeconds = 1
+	testUpdateDelaySeconds         = 15
 )
 
 func TestUpdaterService_UpdateLatestRelease(t *testing.T) {
-	c := gomock.NewController(t)
+	r := require.New(t)
 
-	rg := rm.NewMockClient(c)
-	is := im.NewMockClient(c)
-	updater := NewUpdaterService(context.Background(), rg, is, "8080", false, testDefaultCheckIntervalSeconds)
+	registryClient := rm.NewMockClient(gomock.NewController(t))
+	releaseClient := im.NewMockClient(gomock.NewController(t))
+	updater := NewUpdaterService(
+		context.Background(), registryClient, releaseClient, "8080", false,
+		testUpdateDelaySeconds, testUpdateCheckIntervalSeconds,
+	)
 
-	rg.EXPECT().GetScannerNodeVersion().Return("reference", nil).Times(1)
-	is.EXPECT().GetReleaseManifest(gomock.Any(), "reference").Return(&release.ReleaseManifest{}, nil).Times(1)
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference", nil).Times(1)
+	releaseClient.EXPECT().GetReleaseManifest(gomock.Any(), "reference").Return(&release.ReleaseManifest{}, nil).
+		Times(1)
 	err := updater.updateLatestRelease()
-	assert.NoError(t, err)
+	r.NoError(err)
 }
 
 func TestUpdaterService_UpdateLatestReleaseCached(t *testing.T) {
-	c := gomock.NewController(t)
-	rg := rm.NewMockClient(c)
-	is := im.NewMockClient(c)
-	updater := NewUpdaterService(context.Background(), rg, is, "8080", false, testDefaultCheckIntervalSeconds)
+	r := require.New(t)
 
-	// update twice
-	rg.EXPECT().GetScannerNodeVersion().Return("reference", nil).Times(2)
+	registryClient := rm.NewMockClient(gomock.NewController(t))
+	releaseClient := im.NewMockClient(gomock.NewController(t))
+	updater := NewUpdaterService(
+		context.Background(), registryClient, releaseClient, "8080", false,
+		testUpdateDelaySeconds, testUpdateCheckIntervalSeconds,
+	)
 
-	// only call ipfs once (because value is the same)
-	is.EXPECT().GetReleaseManifest(gomock.Any(), "reference").Return(&release.ReleaseManifest{}, nil).Times(1)
-	assert.NoError(t, updater.updateLatestRelease())
-	assert.NoError(t, updater.updateLatestRelease())
+	// request latest version ref twice
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference", nil).Times(2)
+	// only get release manifest once (because release ref value is the same)
+	releaseClient.EXPECT().GetReleaseManifest(gomock.Any(), "reference").Return(&release.ReleaseManifest{}, nil).
+		Times(1)
+
+	r.NoError(updater.updateLatestRelease())
+	r.NoError(updater.updateLatestRelease())
 }
 
 func TestUpdaterService_UpdateLatestReleaseNotCached(t *testing.T) {
-	c := gomock.NewController(t)
-	rg := rm.NewMockClient(c)
-	is := im.NewMockClient(c)
-	updater := NewUpdaterService(context.Background(), rg, is, "8080", false, testDefaultCheckIntervalSeconds)
+	r := require.New(t)
+
+	registryClient := rm.NewMockClient(gomock.NewController(t))
+	releaseClient := im.NewMockClient(gomock.NewController(t))
+	updater := NewUpdaterService(
+		context.Background(), registryClient, releaseClient, "8080", false,
+		testUpdateDelaySeconds, testUpdateCheckIntervalSeconds,
+	)
 
 	// update twice
-	rg.EXPECT().GetScannerNodeVersion().Return("reference1", nil).Times(1)
-	rg.EXPECT().GetScannerNodeVersion().Return("reference2", nil).Times(1)
 
-	// only call ipfs once (because value is the same)
-	is.EXPECT().GetReleaseManifest(gomock.Any(), "reference1").Return(&release.ReleaseManifest{}, nil).Times(1)
-	is.EXPECT().GetReleaseManifest(gomock.Any(), "reference2").Return(&release.ReleaseManifest{}, nil).Times(1)
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference1", nil).Times(1)
+	releaseClient.EXPECT().GetReleaseManifest(gomock.Any(), "reference1").Return(&release.ReleaseManifest{}, nil).Times(1)
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference2", nil).Times(1)
+	releaseClient.EXPECT().GetReleaseManifest(gomock.Any(), "reference2").Return(&release.ReleaseManifest{}, nil).Times(1)
 
-	assert.NoError(t, updater.updateLatestRelease())
-	assert.NoError(t, updater.updateLatestRelease())
+	r.NoError(updater.updateLatestRelease())
+	r.NoError(updater.updateLatestRelease())
+}
+
+func TestUpdaterService_UpdateLatestReleaseAbort(t *testing.T) {
+	r := require.New(t)
+
+	registryClient := rm.NewMockClient(gomock.NewController(t))
+	releaseClient := im.NewMockClient(gomock.NewController(t))
+	updater := NewUpdaterService(
+		context.Background(), registryClient, releaseClient, "8080", false,
+		testUpdateDelaySeconds, testUpdateCheckIntervalSeconds,
+	)
+
+	initalLatestRef := updater.latestReference
+
+	// get new version
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference1", nil).Times(1)
+	releaseClient.EXPECT().GetReleaseManifest(gomock.Any(), "reference1").Return(&release.ReleaseManifest{}, nil).
+		Times(1)
+	// receive new ref during the async wait
+	registryClient.EXPECT().GetScannerNodeVersion().Return("reference2", nil).Times(1)
+	r.NoError(updater.updateLatestReleaseWithDelay(updater.updateDelay))
+
+	// update should be ineffective and be aborted
+	r.Equal(initalLatestRef, updater.latestReference)
 }
