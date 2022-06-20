31ba93981085a95da68509323b442a48dc1bcc5e
Caner Çıdam <canercidam01@gmail.com>
2022-06-21 00:09:24 +0300

    add logging line

--- services/publisher/publisher.go
+++ services/publisher/publisher.go
@@ -182,6 +182,9 @@ func (pub *Publisher) publishNextBatch(batch *protocol.AlertBatch) error {
 		if err != nil {
 			log.WithError(err).Error("failed to send private alerts")
 		}
+		if alertList != nil {
+			log.WithField("count", len(alertList.Alerts)).Info("successfully sent private alerts")
+		}
 		return err
 	}
 
