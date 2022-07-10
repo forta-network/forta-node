231f72402f70c2044acd097c7d9a9825be475700
ali <aliomerkeser@gmail.com>
2022-07-10 18:09:01 +0300

    migrate jwt-provider cmd to new package

--- cmd/jwt-provider/main.go
+++ cmd/jwt-provider/main.go
@@ -2,16 +2,16 @@ package jwt_provider
 
 import (
 	"context"
-	
+
 	"github.com/forta-network/forta-core-go/clients/health"
 	"github.com/forta-network/forta-node/config"
 	"github.com/forta-network/forta-node/healthutils"
 	"github.com/forta-network/forta-node/services"
-	botjwt "github.com/forta-network/forta-node/services/bot-jwt"
+	jwt_provider "github.com/forta-network/forta-node/services/jwt-provider"
 )
 
-func initJWTProvider(cfg config.Config) (*botjwt.JWTProvider, error) {
-	return botjwt.NewJWTProvider(cfg)
+func initJWTProvider(cfg config.Config) (*jwt_provider.JWTProvider, error) {
+	return jwt_provider.NewJWTProvider(cfg)
 }
 
 func initServices(ctx context.Context, cfg config.Config) ([]services.Service, error) {
@@ -19,7 +19,7 @@ func initServices(ctx context.Context, cfg config.Config) ([]services.Service, e
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return []services.Service{
 		health.NewService(
 			ctx, "", healthutils.DefaultHealthServerErrHandler,
@@ -31,12 +31,12 @@ func initServices(ctx context.Context, cfg config.Config) ([]services.Service, e
 
 func summarizeReports(reports health.Reports) *health.Report {
 	summary := health.NewSummary()
-	
+
 	apiErr, ok := reports.NameContains("service.jwt-provider.api")
 	if ok && len(apiErr.Details) > 0 {
 		summary.Addf("last time the api failed with error '%s'.", apiErr.Details)
 	}
-	
+
 	return summary.Finish()
 }
 
