d2607754756c42cb1670fa9e02759b6580092a63
ali <aliomerkeser@gmail.com>
2022-07-28 13:54:53 +0300

    fix expected container count for test

--- config/containers.go
+++ config/containers.go
@@ -12,18 +12,19 @@ var (
 	DockerSupervisorImage = "forta-network/forta-node:latest"
 	DockerUpdaterImage    = "forta-network/forta-node:latest"
 	UseDockerImages       = "local"
-	
-	DockerSupervisorManagedContainers = 5
+
+	DockerSupervisorManagedContainers = 6
 	DockerUpdaterContainerName        = fmt.Sprintf("%s-updater", ContainerNamePrefix)
 	DockerSupervisorContainerName     = fmt.Sprintf("%s-supervisor", ContainerNamePrefix)
 	DockerNatsContainerName           = fmt.Sprintf("%s-nats", ContainerNamePrefix)
 	DockerIpfsContainerName           = fmt.Sprintf("%s-ipfs", ContainerNamePrefix)
 	DockerScannerContainerName        = fmt.Sprintf("%s-scanner", ContainerNamePrefix)
+	DockerInspectorContainerName      = fmt.Sprintf("%s-inspector", ContainerNamePrefix)
 	DockerJSONRPCProxyContainerName   = fmt.Sprintf("%s-json-rpc", ContainerNamePrefix)
 	DockerJWTProviderContainerName    = fmt.Sprintf("%s-jwt-provider", ContainerNamePrefix)
-	
+
 	DockerNetworkName = DockerScannerContainerName
-	
+
 	DefaultContainerFortaDirPath = "/.forta"
 	DefaultContainerConfigPath   = path.Join(DefaultContainerFortaDirPath, DefaultConfigFileName)
 	DefaultContainerKeyDirPath   = path.Join(DefaultContainerFortaDirPath, DefaultKeysDirName)
