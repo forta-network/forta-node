c1532b6fb76c43c217cf47ace83a4f2d48bfe789
Caner Çıdam <canercidam01@gmail.com>
2023-03-01 17:45:27 +0300

    remove deprecated ScannerRegistry-related commands

--- cmd/cmd_tx.go
+++ cmd/cmd_tx.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"math/big"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/fatih/color"
 	"github.com/forta-network/forta-core-go/registry"
@@ -120,123 +117,4 @@ func makeArgsTuple(scannerAddr string, poolID int64, chainID int, ts int64) stri
 	tuple[4] = (*hexutil.Big)(big.NewInt(int64(ts))).String()
 	b, _ := json.Marshal(tuple)
 	return string(b)
-}
-
-func handleFortaEnable(cmd *cobra.Command, args []string) error {
-	scannerKey, err := security.LoadKeyWithPassphrase(cfg.KeyDirPath, cfg.Passphrase)
-	if err != nil {
-		return fmt.Errorf("failed to load scanner key: %v", err)
-	}
-	scannerPrivateKey := scannerKey.PrivateKey
-	scannerAddressStr := scannerKey.Address.Hex()
-
-	regClient, err := store.GetRegistryClient(context.Background(), cfg, registry.ClientConfig{
-		JsonRpcUrl: cfg.Registry.JsonRpc.Url,
-		ENSAddress: cfg.ENSConfig.ContractAddress,
-		Name:       "registry-client",
-		PrivateKey: scannerPrivateKey,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to create registry client: %v", err)
-	}
-	regClient.SetRegistryChainID(cfg.Registry.ChainID)
-
-	color.Yellow("Sending a transaction to enable your scan node...\n")
-
-	txHash, err := regClient.EnableScanner(registry.ScannerPermissionSelf, scannerAddressStr)
-	if err != nil && strings.Contains(err.Error(), "insufficient funds") {
-		yellowBold("This action requires Polygon (Mainnet) MATIC. Have you funded your address %s yet?\n", scannerAddressStr)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to send the transaction: %v", err)
-	}
-
-	greenBold("Successfully sent the transaction!\n\n")
-	whiteBold("https://polygonscan.com/tx/%s\n", txHash)
-
-	return nil
-}
-
-// DEPRECATED COMMANDS:
-
-func handleFortaRegister(cmd *cobra.Command, args []string) error {
-	ownerAddressStr, err := cmd.Flags().GetString("owner-address")
-	if err != nil {
-		return err
-	}
-	if !common.IsHexAddress(ownerAddressStr) {
-		return errors.New("invalid owner address provided")
-	}
-
-	scannerKey, err := security.LoadKeyWithPassphrase(cfg.KeyDirPath, cfg.Passphrase)
-	if err != nil {
-		return fmt.Errorf("failed to load scanner key: %v", err)
-	}
-	scannerPrivateKey := scannerKey.PrivateKey
-	scannerAddressStr := scannerKey.Address.Hex()
-
-	if strings.EqualFold(scannerAddressStr, ownerAddressStr) {
-		redBold("Scanner and owner cannot be the same identity! Please provide a different wallet address of your own.\n")
-	}
-
-	regClient, err := store.GetRegistryClient(context.Background(), cfg, registry.ClientConfig{
-		JsonRpcUrl: cfg.Registry.JsonRpc.Url,
-		ENSAddress: cfg.ENSConfig.ContractAddress,
-		Name:       "registry-client",
-		PrivateKey: scannerPrivateKey,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to create registry client: %v", err)
-	}
-	regClient.SetRegistryChainID(cfg.Registry.ChainID)
-
-	color.Yellow(fmt.Sprintf("Sending a transaction to register your scan node to chain %d...\n", cfg.ChainID))
-
-	txHash, err := regClient.RegisterScannerOld(ownerAddressStr, int64(cfg.ChainID), "")
-	if err != nil && strings.Contains(err.Error(), "insufficient funds") {
-		yellowBold("This action requires Polygon (Mainnet) MATIC. Have you funded your address %s yet?\n", scannerAddressStr)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to send the transaction: %v", err)
-	}
-
-	greenBold("Successfully sent the transaction!\n\n")
-	whiteBold("Please ensure that https://polygonscan.com/tx/%s succeeds before you do 'forta run'. This can take a while depending on the network load.\n", txHash)
-
-	return nil
-}
-
-func handleFortaDisable(cmd *cobra.Command, args []string) error {
-	scannerKey, err := security.LoadKeyWithPassphrase(cfg.KeyDirPath, cfg.Passphrase)
-	if err != nil {
-		return fmt.Errorf("failed to load scanner key: %v", err)
-	}
-	scannerPrivateKey := scannerKey.PrivateKey
-	scannerAddressStr := scannerKey.Address.Hex()
-
-	regClient, err := store.GetRegistryClient(context.Background(), cfg, registry.ClientConfig{
-		JsonRpcUrl: cfg.Registry.JsonRpc.Url,
-		ENSAddress: cfg.ENSConfig.ContractAddress,
-		Name:       "registry-client",
-		PrivateKey: scannerPrivateKey,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to create registry client: %v", err)
-	}
-	regClient.SetRegistryChainID(cfg.Registry.ChainID)
-
-	color.Yellow("Sending a transaction to disable your scan node...\n")
-
-	txHash, err := regClient.DisableScanner(registry.ScannerPermissionSelf, scannerAddressStr)
-	if err != nil && strings.Contains(err.Error(), "insufficient funds") {
-		yellowBold("This action requires Polygon (Mainnet) MATIC. Have you funded your address %s yet?\n", scannerAddressStr)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to send the transaction: %v", err)
-	}
-
-	greenBold("Successfully sent the transaction!\n\n")
-	whiteBold("https://polygonscan.com/tx/%s\n", txHash)
-
-	return nil
 }
