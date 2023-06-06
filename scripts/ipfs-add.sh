698e7f270a420a56f110547ce029ecddffd37ea5
Caner Çıdam <canercidam01@gmail.com>
2023-06-06 17:40:02 +0300

    use jq instead of sed to get the ipfs cid from the upload output

--- scripts/ipfs-add.sh
+++ scripts/ipfs-add.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-curl -s -X POST -F file=@$1 https://ipfs.forta.network/api/v0/add | sed 's/.*Qm/Qm/g' |sed 's/\".*$//g'
+curl -s -X POST -F file=@"$1" https://ipfs.forta.network/api/v0/add | jq -r .Hash
