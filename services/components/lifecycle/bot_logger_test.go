555828ba4045856654f8f0aa9093ae3169a50d98
Marcos <marcoscarlomagno1@gmail.com>
2023-09-19 22:26:43 -0300

    added tests

--- services/components/lifecycle/bot_logger_test.go
+++ services/components/lifecycle/bot_logger_test.go
@@ -2,12 +2,15 @@ package lifecycle
 
 import (
 	"context"
+	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/docker/docker/api/types"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/forta-network/forta-core-go/clients/agentlogs"
 	"github.com/forta-network/forta-core-go/security"
+	"github.com/forta-network/forta-node/clients/docker"
 	mock_clients "github.com/forta-network/forta-node/clients/mocks"
 	mock_containers "github.com/forta-network/forta-node/services/components/containers/mocks"
 	"github.com/golang/mock/gomock"
@@ -57,9 +60,8 @@ func (s *BotLoggerSuite) TestSendBotLogs() {
 		s.botClient, s.dockerClient, s.key,
 		func(agents agentlogs.Agents, authToken string) error {
 			s.r.Equal(2, len(agents))
-			s.r.Equal("bot1", agents[0].ID)
-			s.r.Equal("bot2", agents[1].ID)
-			// TODO: test sendLogs = 1 and sendLogs = 0 conditions
+			s.r.Equal("bot1ID", agents[0].ID)
+			s.r.Equal("bot2ID", agents[1].ID)
 			return nil
 		},
 	)
@@ -69,13 +71,130 @@ func (s *BotLoggerSuite) TestSendBotLogs() {
 		{
 			ID:    "bot1",
 			Image: "forta/bot:latest",
+			Labels: map[string]string{
+				docker.LabelFortaSettingsAgentLogsEnable: "true",
+				docker.LabelFortaBotID:                   "bot1ID",
+			},
 		},
 		{
 			ID:    "bot2",
 			Image: "forta/bot:latest",
+			Labels: map[string]string{
+				docker.LabelFortaSettingsAgentLogsEnable: "true",
+				docker.LabelFortaBotID:                   "bot2ID",
+			},
 		},
 	}
+	s.dockerClient.EXPECT().GetContainerLogs(
+		ctx, "bot1",
+		strconv.Itoa(defaultAgentLogTailLines),
+		defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+	).Return("some log", nil).Times(1)
+
+	s.dockerClient.EXPECT().GetContainerLogs(
+		ctx, "bot2",
+		strconv.Itoa(defaultAgentLogTailLines),
+		defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+	).Return("some log", nil).Times(1)
+
+	s.botClient.EXPECT().LoadBotContainers(ctx).Return(mockContainers, nil)
+	botLogger.SendBotLogs(ctx)
+}
+
+// should fail if there is an error loading
+// bot containers
+func (s *BotLoggerSuite) TestLoadBotContainersError() {
+	botLogger := NewBotLogger(
+		s.botClient, s.dockerClient, s.key,
+		func(agents agentlogs.Agents, authToken string) error {
+			return nil
+		},
+	)
+	ctx := context.Background()
+
+	mockContainers := []types.Container{}
+
+	s.botClient.EXPECT().LoadBotContainers(ctx).Return(mockContainers, errors.New("test"))
+	s.r.Error(botLogger.SendBotLogs(ctx), "failed to load the bot containers: test")
+}
+
+// Should not send agent logs if fails
+// to get container logs but continue processing
+func (s *BotLoggerSuite) TestGetContainerLogsError() {
+	botLogger := NewBotLogger(
+		s.botClient, s.dockerClient, s.key,
+		func(agents agentlogs.Agents, authToken string) error {
+			s.r.Equal(1, len(agents))
+			s.r.Equal("bot2ID", agents[0].ID)
+			s.r.Equal("some log", agents[0].Logs)
+			return nil
+		},
+	)
+	ctx := context.Background()
+
+	mockContainers := []types.Container{
+		{
+			ID:    "bot1",
+			Image: "forta/bot:latest",
+			Labels: map[string]string{
+				docker.LabelFortaSettingsAgentLogsEnable: "true",
+			},
+		},
+		{
+			ID:    "bot2",
+			Image: "forta/bot:latest",
+			Labels: map[string]string{
+				docker.LabelFortaSettingsAgentLogsEnable: "true",
+				docker.LabelFortaBotID:                   "bot2ID",
+			},
+		},
+	}
+
 	s.botClient.EXPECT().LoadBotContainers(ctx).Return(mockContainers, nil)
 
+	s.dockerClient.EXPECT().GetContainerLogs(
+		ctx, "bot1",
+		strconv.Itoa(defaultAgentLogTailLines),
+		defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+	).Return("", errors.New("test")).Times(1)
+
+	s.dockerClient.EXPECT().GetContainerLogs(
+		ctx, "bot2",
+		strconv.Itoa(defaultAgentLogTailLines),
+		defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+	).Return("some log", nil).Times(1)
+
 	botLogger.SendBotLogs(ctx)
+}
+
+// Fails sending agent logs
+func (s *BotLoggerSuite) TestFailsToSendLogs() {
+	botLogger := NewBotLogger(
+		s.botClient, s.dockerClient, s.key,
+		func(agents agentlogs.Agents, authToken string) error {
+			return errors.New("test")
+		},
+	)
+	ctx := context.Background()
+
+	mockContainers := []types.Container{
+		{
+			ID:    "bot1",
+			Image: "forta/bot:latest",
+			Labels: map[string]string{
+				docker.LabelFortaSettingsAgentLogsEnable: "true",
+				docker.LabelFortaBotID:                   "bot1ID",
+			},
+		},
+	}
+
+	s.botClient.EXPECT().LoadBotContainers(ctx).Return(mockContainers, nil)
+
+	s.dockerClient.EXPECT().GetContainerLogs(
+		ctx, "bot1",
+		strconv.Itoa(defaultAgentLogTailLines),
+		defaultAgentLogAvgMaxCharsPerLine*defaultAgentLogTailLines,
+	).Return("some log", nil).Times(1)
+
+	s.r.Error(botLogger.SendBotLogs(ctx), "failed to send agent logs: test")
 }
