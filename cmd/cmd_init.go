d5301b84d5c81b8e1aa656b4ac5ac035370fe6c2
Caner Çıdam <canercidam01@gmail.com>
2023-12-20 11:46:56 +0300

    simplify initialization

--- cmd/cmd_init.go
+++ cmd/cmd_init.go
@@ -1,13 +1,11 @@
 package cmd
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
-	"text/template"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/fatih/color"
@@ -29,15 +27,7 @@ func handleFortaInit(cmd *cobra.Command, args []string) error {
 	}
 
 	if !isConfigFileInitialized() {
-		tmpl, err := template.New("config-template").Parse(defaultConfig)
-		if err != nil {
-			return err
-		}
-		var buf bytes.Buffer
-		if err := tmpl.Execute(&buf, nil); err != nil {
-			return err
-		}
-		if err := os.WriteFile(cfg.ConfigFilePath(), buf.Bytes(), 0644); err != nil {
+		if err := os.WriteFile(cfg.ConfigFilePath(), []byte(defaultConfig), 0644); err != nil {
 			return err
 		}
 	}
